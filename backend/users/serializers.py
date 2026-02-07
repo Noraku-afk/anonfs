b997f5b6972c33c589bdbe12b301442205b12fd0
Devansh <devansh@Devanshs-MacBook-Air.local>
2026-02-07 23:44:52 +0530

    Add username claim to JWT token

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # token['email'] = user.email
+
+        return token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
